8c951103e52412882256b6b606c13fd286ba9f94
GalacticGaming1225 <42882305+GalacticGaming1225@users.noreply.github.com>
2022-01-03 22:32:31 -0500

    s
    a

---
+++ maps/anzus_lifev3.js
@@ -0,0 +1,196 @@
+Arma3Map.Maps.anzus_lifev3 = {
+  CRS: MGRS_CRS(0.012375, 0.012375, 203),
+  "worldName": "anzus_lifev3",
+  "tilePattern": "/maps/anzus_lifev3/{z}/{x}/{y}.png",
+  "maxZoom": 6,
+  "minZoom": 0,
+  "defaultZoom": 2,
+  "attribution": "&copy; Bohemia Interactive",
+  "tileSize": 203,
+  "center": [
+    8192,
+    8192
+  ],
+  "worldSize": 16384.0,
+  "preview": null,
+  "dlc": null,
+  "title": "Anzus Life v3",
+  "steamWorkshop": null,
+  "cities": [
+    {
+      "name": "Winter Mansion",
+      "x": 5782.63,
+      "y": 4387.98
+    },
+    {
+      "name": "Coleman Bay",
+      "x": 4268.75,
+      "y": 5841.6
+    },
+    {
+      "name": "Newman Slums",
+      "x": 6551.5,
+      "y": 4526.53
+    },
+    {
+      "name": "Redwater",
+      "x": 7018.34,
+      "y": 5838.27
+    },
+    {
+      "name": "Forehead Trailer Park",
+      "x": 5190.86,
+      "y": 8081.03
+    },
+    {
+      "name": "Elliot B Trailer Park",
+      "x": 7059.69,
+      "y": 7566.06
+    },
+    {
+      "name": "Shreman Trailer Park",
+      "x": 5925.75,
+      "y": 8231.93
+    },
+    {
+      "name": "Fort Snowflake",
+      "x": 8324.31,
+      "y": 5555.37
+    },
+    {
+      "name": "Scottsdale",
+      "x": 7419.53,
+      "y": 7467.93
+    },
+    {
+      "name": "Petal Point",
+      "x": 7920.62,
+      "y": 7242.77
+    },
+    {
+      "name": "Tilted Trailers",
+      "x": 6255.84,
+      "y": 8576.91
+    },
+    {
+      "name": "ST Snovle's Island",
+      "x": 6197.44,
+      "y": 9242.3
+    },
+    {
+      "name": "Alexis Beach",
+      "x": 6664.58,
+      "y": 9768.65
+    },
+    {
+      "name": "Ericsson Square",
+      "x": 6036.96,
+      "y": 10161.2
+    },
+    {
+      "name": "Kingston",
+      "x": 8134.99,
+      "y": 9297.52
+    },
+    {
+      "name": "Snowside",
+      "x": 10259.9,
+      "y": 3903.12
+    },
+    {
+      "name": "Higgins Hights",
+      "x": 5574.56,
+      "y": 10451.0
+    },
+    {
+      "name": "Clark Trailer Park",
+      "x": 10340.6,
+      "y": 6912.49
+    },
+    {
+      "name": "Boyd Bay",
+      "x": 830.579,
+      "y": 1240.27
+    },
+    {
+      "name": "Slade Hill",
+      "x": 10705.6,
+      "y": 6752.63
+    },
+    {
+      "name": "Tigerwood Bay",
+      "x": 6651.05,
+      "y": 10972.8
+    },
+    {
+      "name": "Sanchez Trailer Park",
+      "x": 5805.86,
+      "y": 11243.2
+    },
+    {
+      "name": "Jones Road",
+      "x": 10254.2,
+      "y": 8717.04
+    },
+    {
+      "name": "Winter Trailer Park",
+      "x": 5448.22,
+      "y": 11388.8
+    },
+    {
+      "name": "Atron Residential",
+      "x": 7759.64,
+      "y": 10840.0
+    },
+    {
+      "name": "Elkriver",
+      "x": 10944.0,
+      "y": 7838.89
+    },
+    {
+      "name": "Chapman Stream",
+      "x": 8611.03,
+      "y": 11315.9
+    },
+    {
+      "name": "Margeson Loop",
+      "x": 11111.4,
+      "y": 9089.28
+    },
+    {
+      "name": "Knight Rest Stop",
+      "x": 9444.22,
+      "y": 11350.5
+    },
+    {
+      "name": "Bergville",
+      "x": 9873.61,
+      "y": 11386.0
+    },
+    {
+      "name": "Jacksonville",
+      "x": 10751.9,
+      "y": 11190.7
+    },
+    {
+      "name": "Westwood",
+      "x": 11472.2,
+      "y": 11911.2
+    },
+    {
+      "name": "Westwood Trailer Park",
+      "x": 11681.4,
+      "y": 11874.4
+    },
+    {
+      "name": "Cage Trailer Park",
+      "x": 12636.5,
+      "y": 10855.3
+    },
+    {
+      "name": "Arbosa Shores",
+      "x": 13129.3,
+      "y": 12361.5
+    }
+  ]
+};
